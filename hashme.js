977a7cb0efb788f58e8b7ed30e3678e0bc889754
Tanya <mellene@gmail.com>
2018-05-24 11:24:06 -0600

    more config jwt hashing

---
+++ hashme.js
@@ -0,0 +1,13 @@
+//hashing password:
+//go to terminal, run this file and do what console log says
+//type your password, you will get hush
+//put hash in seeds users
+const { hashSync } = require('bcryptjs')
+const SALT_ROUNDS = 10
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node hashme.js [CLEAR TEXT YOU WANT TO HASH]')
+}
+else {
+  console.log(hashSync(process.argv[2]))
+}
